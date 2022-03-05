6703e6b6d6efbc7bd7a70b8007481ec49f840266
jessica <jessicathaynnara@gmail.com>
2022-03-05 07:56:44 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
-import store from './store/store';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 function App() {
   return (
-    <Provider store={ store }>
-      <Switch>
-        <Route exact path="/carteira" component={ Wallet } />
-        <Route exact path="/" component={ Login } />
-      </Switch>
-    </Provider>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/carteira" component={ Wallet } />
+    </Switch>
   );
 }
 
