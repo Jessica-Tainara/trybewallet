3c75100adf2beec3d56553dc117c8b9fe205e73b
jessica <jessicathaynnara@gmail.com>
2022-03-05 09:41:58 -0300

    Requisito 7

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -10,18 +10,23 @@ class Wallet extends React.Component {
     this.state = {
       value: 0,
       description: '',
-      currency: '',
-      method: '',
-      tag: '',
+      currency: 'USD',
+      method: 'Cartão de crédito',
+      tag: 'Alimentação',
       id: 0,
       total: 0.00,
       currencies: [],
+      listExpenses: [],
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleClickSubmit = this.handleClickSubmit.bind(this);
   }
 
   componentDidMount() {
+    const { expenses } = this.props;
+    this.setState({
+      listExpenses: expenses,
+    });
     fetch('https://economia.awesomeapi.com.br/json/all')
       .then((response) => response.json())
       .then((data) => {
@@ -50,11 +55,6 @@ class Wallet extends React.Component {
     fetch('https://economia.awesomeapi.com.br/json/all')
       .then((response) => response.json())
       .then((data) => {
-        this.setState({
-          id: id + 1,
-          total: parseFloat(total) + parseFloat(value * data[currency].ask),
-          value: 0,
-        });
         dispatch(addExpense({
           id,
           value,
@@ -64,12 +64,19 @@ class Wallet extends React.Component {
           tag,
           exchangeRates: data,
         }));
+        const { expenses } = this.props;
+        this.setState({
+          id: id + 1,
+          total: (parseFloat(total) + parseFloat(value * data[currency].ask)).toFixed(2),
+          value: 0,
+          listExpenses: expenses,
+        });
       });
   }
 
   render() {
     const { email } = this.props;
-    const { total, currencies, value, method } = this.state;
+    const { total, currencies, value, method, tag, listExpenses } = this.state;
     return (
       <div>
         <header>
@@ -121,9 +128,9 @@ class Wallet extends React.Component {
               value={ method }
               onChange={ this.handleInputChange }
             >
-              <option value="Dinheiro">Dinheiro</option>
               <option value="Cartão de crédito">Cartão de crédito</option>
               <option value="Cartão de débito">Cartão de débito</option>
+              <option value="Dinheiro">Dinheiro</option>
             </select>
           </label>
           <label htmlFor="tag">
@@ -133,6 +140,7 @@ class Wallet extends React.Component {
               id="tag"
               name="tag"
               onChange={ this.handleInputChange }
+              value={ tag }
             >
               <option value="Alimentação">Alimentação</option>
               <option value="Lazer">Lazer</option>
@@ -162,6 +170,35 @@ class Wallet extends React.Component {
               <th>Editar/Excluir</th>
             </tr>
           </thead>
+          <tbody>
+            {listExpenses
+              .map((item) => {
+                const {
+                  id, description, tag: Tag, method: Method, value: Val,
+                  exchangeRates: rates, currency } = item;
+                return (
+                  <tr key={ id }>
+                    <td role="cell">{description}</td>
+                    <td role="cell">{Tag}</td>
+                    <td role="cell">{Method}</td>
+                    <td role="cell">{parseFloat(Val) % 1 === 0 ? `${Val}.00` : Val}</td>
+                    <td role="cell">{rates[currency].name}</td>
+                    <td role="cell">{parseFloat(rates[currency].ask).toFixed(2)}</td>
+                    <td role="cell">{rates[currency].ask * Val}</td>
+                    <td role="cell">Real</td>
+                    <td role="cell">
+                      <button
+                        type="button"
+                        data-testid="delete-btn"
+                      >
+                        Excluir
+                      </button>
+                    </td>
+                  </tr>
+                );
+              }) }
+
+          </tbody>
         </table>
       </div>);
   }
@@ -169,8 +206,10 @@ class Wallet extends React.Component {
 Wallet.propTypes = {
   email: PropTypes.string.isRequired,
   dispatch: PropTypes.func.isRequired,
+  expenses: PropTypes.string.isRequired,
 };
 const mapStateToProps = (state) => ({
-  email: state.user.email });
+  email: state.user.email,
+  expenses: state.wallet.expenses });
 
 export default connect(mapStateToProps)(Wallet);
